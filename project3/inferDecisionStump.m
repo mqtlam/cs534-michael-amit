4ec2d355a5aa2e650a8204afd7b34b3edc2b0b1d
michaelqtlam@gmail.com <michaelqtlam@gmail.com@645c4e7a-58d9-daef-8f8a-0179c6c71a83>
2013-05-25 00:32:57 +0000

    Fixed the decision tree and consequently fixed the AdaBoost code. The new decision tree hypothesis is different! Previous code commented out just in case, but will be deleted before turning in.

--- project3/inferDecisionStump.m
+++ project3/inferDecisionStump.m
@@ -1,15 +1,29 @@
 function [ predictedLabels ] = inferDecisionStump( data, hypothesis )
 %INFERDECISIONSTUMP Given data and hypothesis, predict labels.
 %   data:               data matrix; rows = examples, cols = features
-%   hypothesis:         index on feature to decide
-%                       if positive index, just split on feature
-%                       if negative index, split on feature and flip values
+%   hypothesis:         decision stump hypothesis
+%                           .feature is feature to split (feature index)
+%                           .neg is label of negative branch/leaf (0 or 1)
+%                           .pos is label of positive branch/leaf (0 or 1)
 %   predictedLabels:    predicted labels from decision stump
 
-index = abs(hypothesis);
-test = hypothesis > 0;
+[nExamples, ~] = size(data);
+index = hypothesis.feature;
 
-predictedLabels = double(data(:, index) == test);
+if hypothesis.neg == 0 && hypothesis.pos == 1
+    predictedLabels = double(data(:, index) == 1);
+elseif hypothesis.neg == 1 && hypothesis.pos == 0
+    predictedLabels = double(data(:, index) == 0);
+elseif hypothesis.neg == 1 && hypothesis.pos == 1
+    predictedLabels = ones(nExamples, 1);
+elseif hypothesis.neg == 0 && hypothesis.pos == 0
+    predictedLabels = zeros(nExamples, 1);
+end
+
+% index = abs(hypothesis);
+% test = hypothesis > 0;
+% 
+% predictedLabels = double(data(:, index) == test);
 
 end
 
