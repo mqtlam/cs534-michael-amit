e9fbf2add08c79f986c81f64b5b4843a2f96a82e
michaelqtlam@gmail.com <michaelqtlam@gmail.com@645c4e7a-58d9-daef-8f8a-0179c6c71a83>
2013-05-28 05:12:53 +0000

    Bagging inference corrected. Comment changes.

--- trunk/project3/inferBagging.m
+++ trunk/project3/inferBagging.m
@@ -1,32 +1,25 @@
 function [ predictedLabels ] = inferBagging( data, hypothesisStruct )
 %INFERBAGGING Classify with Bagging hypothesis and majority vote.
 %   data:               data matrix; rows = examples, cols = features
-%   hypothesisStruct:   struct containing hypotheses and weights
+%   hypothesisStruct:   struct containing hypotheses
 %                       each hypothesis:
 %                           .h is decision stump hypothesis
-%                           .alpha is weight for weighted vote in the end
 %   predictedLabels:    final predicted labels
 
 %% setup
 [nExamples, ~] = size(data);
 nEnsembles = size(hypothesisStruct.h, 1);
 
-%% final classifier
-% returns linear combination of weak classifiers and weights
-voting = zeros(size(data,2),1);
+%% final classifier: returns majority vote
+positives = zeros(nExamples, 1);
+negatives = zeros(nExamples, 1);
 for i = 1:nEnsembles
-    voting(hypothesisStruct.h{i}.feature) = voting(hypothesisStruct.h{i}.feature) + 1; 
+    h = inferDecisionStump(data, hypothesisStruct.h{i});
+    positives = positives + double(h == 1);
+    negatives = negatives + double(h == 0);
 end
 
-[~, index] = max(voting);
-
-for i = 1:nEnsembles
-    if(hypothesisStruct.h{i}.feature == index)
-        h = hypothesisStruct.h{i};
-    end
-end
-
-predictedLabels = inferDecisionStump(data, h);
+predictedLabels = double(positives > negatives);
 
 end
 
