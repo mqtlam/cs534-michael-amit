1c444c2d4faf987334ee5d6a3570668e3066ac35
michaelqtlam@gmail.com <michaelqtlam@gmail.com@645c4e7a-58d9-daef-8f8a-0179c6c71a83>
2013-05-28 05:07:49 +0000

    Tidied up learn bagging code.

--- project3/learnBagging.m
+++ project3/learnBagging.m
@@ -9,48 +9,20 @@ function [ hypothesisStruct ] = learnBagging( data, nEnsembles )
 %                           .alpha is weight for weighted vote in the end
 
 %% setup
-[nExamples, ~] = size(data);
 hypothesisStruct.h = cell(nEnsembles, 1);
-hypothesisStruct.alpha = zeros(nEnsembles, 1);
 
-%Set the number of samples. 'T'
-T = size(data,1);
-
-% set initial weights (distribution) to uniform
-distribution = 1/nExamples*ones(T, 1);
+% set the number of samples. 'T'
+T = size(data, 1);
 
 %% main loop
 for l = 1:nEnsembles
-    
-    
-    % Randomly select 'T' samples from the data for weak classification.
+    % randomly select 'T' samples with replacement from the data
     dataSample = datasample(data, T);
     
+    % keep hypothesis for final classifier
     trainLabels = dataSample(:, 1);
     trainData = dataSample(:, 2:end);
-    
-    % get weak classifier hypothesis and error
-    h_l = learnDecisionStump(trainData, trainLabels, distribution);
-    
-%     predictedLabels = inferDecisionStump(data, h_l);
-%     incorrectMask = double(predictedLabels ~= labels);
-%     e_l = sum(distribution .* incorrectMask);
-%     
-%     % stop if error >= 1/2
-%     if e_l >= 0.5
-%         break;
-%     end
-%     
-%     % compute weight for the weak classifier
-%     alpha_l = 1/2*log((1-e_l)/e_l);
-%     
-%     % update distribution for training data
-%     distribution = distribution .* (incorrectMask*exp(alpha_l)...
-%         + (1-incorrectMask)*exp(-alpha_l));
-%     distribution = distribution ./ sum(distribution);
-    
-    % keep hypothesis and alpha for final classifier
-    hypothesisStruct.h{l} = h_l;
+    hypothesisStruct.h{l} = learnDecisionStump(trainData, trainLabels);
 end
 
 end
