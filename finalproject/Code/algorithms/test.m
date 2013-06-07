aaa79fd4bf628f84f4903394718176aada60ce1a
michaelqtlam@gmail.com <michaelqtlam@gmail.com@645c4e7a-58d9-daef-8f8a-0179c6c71a83>
2013-06-07 22:36:18 +0000

    Updated test script so that it combines all the datasets into one, then does the training/testing.

--- finalproject/Code/algorithms/test.m
+++ finalproject/Code/algorithms/test.m
@@ -1,138 +1,187 @@
 clear;
 
-%% settings
-ENSEMBLE_SIZES = [5, 10, 15, 20, 25, 30];
-TREE_MAX_DEPTHS = [2, 3, 4, 5, 6, 7];
+%% loop over datasets
+%for iter = [2,4]
+    %fprintf('\nUSING DATASET %d...\n\n', iter);
 
-PERCENT_TRAINING = .75; % percentage of dataset allocated for training
-RANDOMIZE_DATASET = 1;  % randomize dataset for training/test split
-FIX_SEED = 1;           % fix random seed or not
+    %% settings
+    ENSEMBLE_SIZES = [5, 10, 15, 20, 25, 30, 35, 40];
+    TREE_MAX_DEPTHS = [0, 1, 2, 3, 4, 5, 6, 7];
 
-if FIX_SEED
-    rng(12345); % fix seed
-end
+    PERCENT_TRAINING = .75; % percentage of dataset allocated for training
+    RANDOMIZE_DATASET = 1;  % randomize dataset for training/test split
+    FIX_SEED = 1;           % fix random seed or not
 
-%% load data
-%load('../ProjectDataset/game2/dataset2.mat');
-load('../ProjectDataset/game4/dataset4.mat');
+    if FIX_SEED
+        rng(12345); % fix seed
+    end
 
-% features
-X = pData(:, 1:end-1);
-[nExamples, nFeatures] = size(X);
-
-% ground truth
-y = pData(:, end);
-y(y == 2) = 0;
-
-% split into training and testing sets
-nTraining = ceil(PERCENT_TRAINING*nExamples);
-nTest = nExamples - nTraining;
-if RANDOMIZE_DATASET
-    augData = shuffle_rows([X, y]);
-    X = augData(:, 1:end-1);
-    y = augData(:, end);
-end
-XTrain = X(1:nTraining, :);
-yTrain = y(1:nTraining);
-XTest = X(nTraining+1:end, :);
-yTest = y(nTraining+1:end);
-
-%% evaluate using AdaBoost on different ensemble sizes
-adaBoostResults = cell(length(ENSEMBLE_SIZES), 1);
-adaBoostTrainErrors = zeros(length(ENSEMBLE_SIZES), 1);
-adaBoostTestErrors = zeros(length(ENSEMBLE_SIZES), 1);
-
-cnt = 1;
-for size = ENSEMBLE_SIZES
-    fprintf('AdaBoost ensemble size: %d\n', size);
+    PAUSE_PLOT = 0;
     
-    h = learnAdaBoost(XTrain, yTrain, size);
-    yTrainPred = inferAdaBoost(XTrain, h);
-    yTestPred = inferAdaBoost(XTest, h);
+    %SAVE_FILENAME = 'dataset2';
+    %SAVE_FILENAME = 'dataset4';
+    %SAVE_FILENAME = sprintf('dataset%d', iter);
+    SAVE_FILENAME = 'dataset';
     
-    eTrain = sum(yTrain ~= yTrainPred)/numel(yTrain);
-    aTrain = 1-eTrain;
-    fprintf('\tAdaBoost training error: %f\n', eTrain);
+    %% load data
+    %load('../ProjectDataset/game2/dataset2.mat');
+    %load('../ProjectDataset/game4/dataset4.mat');
+    %load(sprintf('../ProjectDataset/game%d/dataset%d.mat', iter, iter));
     
-    eTest = sum(yTest ~= yTestPred)/numel(yTest);
-    aTest = 1-eTest;
-    fprintf('\tAdaBoost test error: %f\n', eTest);
-    
-    adaBoostResults{cnt}.ensembleSize = size;
-    adaBoostResults{cnt}.trainAccuracy = aTrain;
-    adaBoostResults{cnt}.trainConfusionMatrix = confusionmatrix(yTrain, yTrainPred);
-    adaBoostResults{cnt}.testAccuracy = aTest;
-    adaBoostResults{cnt}.testConfusionMatrix = confusionmatrix(yTest, yTestPred);
-    
-    adaBoostTrainErrors(cnt) = eTrain;
-    adaBoostTestErrors(cnt) = eTest;
-    
-    cnt = cnt+1;
-end
+    X = [];
+    y = [];
+    for game = [2,3,4]
+        load(sprintf('../ProjectDataset/game%d/dataset%d.mat', game, game));
+        %load(sprintf('../ProjectDataset/game%d/vidnames%d.mat', game, game));
 
-%% evaluate using decision tree
-decisionTreeResults = cell(length(TREE_MAX_DEPTHS), 1);
-decisionTreeTrainErrors = zeros(length(TREE_MAX_DEPTHS), 1);
-decisionTreeTestErrors = zeros(length(TREE_MAX_DEPTHS), 1);
+        % features
+        %X = pData(:, 1:end-1);
+        X = vertcat(X, pData(:, 1:end-1));
+        [nExamples, nFeatures] = size(X);
 
-cnt = 1;
-for depth = TREE_MAX_DEPTHS
-    fprintf('Decision tree max depth: %d\n', depth);
-    
-    dist = 1/nTraining*ones(nTraining, 1);
-    h = learnDecisionTree(XTrain, yTrain, dist, depth);
-    yTrainPred = inferDecisionTree(XTrain, h);
-    yTestPred = inferDecisionTree(XTest, h);
-    
-    eTrain = sum(yTrain ~= yTrainPred)/numel(yTrain);
-    aTrain = 1-eTrain;
-    fprintf('\tDecision tree training error: %f\n', eTrain);
-    
-    eTest = sum(yTest ~= yTestPred)/numel(yTest);
-    aTest = 1-eTest;
-    fprintf('\tDecision tree test error: %f\n', eTest);
-    
-    decisionTreeResults{cnt}.maxDepth = depth;
-    decisionTreeResults{cnt}.trainAccuracy = aTrain;
-    decisionTreeResults{cnt}.trainConfusionMatrix = confusionmatrix(yTrain, yTrainPred);
-    decisionTreeResults{cnt}.testAccuracy = aTest;
-    decisionTreeResults{cnt}.testConfusionMatrix = confusionmatrix(yTest, yTestPred);
-    
-    decisionTreeTrainErrors(cnt) = eTrain;
-    decisionTreeTestErrors(cnt) = eTest;
-    
-    cnt = cnt+1;
-end
+        % ground truth
+        %y = pData(:, end);
+        y = vertcat(y, pData(:, end));
+        y(y == 2) = 0;
+    end
 
-%% plot evaluations
-% training error for adaboost
-plot(ENSEMBLE_SIZES', adaBoostTrainErrors);
-xlabel('Ensemble Size');
-ylabel('Training Error');
-title('AdaBoost: Training Error vs. Ensemble Size');
-pause;
+    % split into training and testing sets
+    nTraining = ceil(PERCENT_TRAINING*nExamples);
+    nTest = nExamples - nTraining;
+    if RANDOMIZE_DATASET
+        augData = shuffle_rows([X, y]);
+        X = augData(:, 1:end-1);
+        y = augData(:, end);
+    end
+    XTrain = X(1:nTraining, :);
+    yTrain = y(1:nTraining);
+    XTest = X(nTraining+1:end, :);
+    yTest = y(nTraining+1:end);
 
-% test error for adaboost
-plot(ENSEMBLE_SIZES', adaBoostTestErrors);
-xlabel('Ensemble Size');
-ylabel('Test Error');
-title('AdaBoost: Test Error vs. Ensemble Size');
-pause;
+    %% evaluate using AdaBoost on different ensemble sizes
+    adaBoostResults = cell(length(ENSEMBLE_SIZES), 1);
+    adaBoostTrainErrors = zeros(length(ENSEMBLE_SIZES), 1);
+    adaBoostTestErrors = zeros(length(ENSEMBLE_SIZES), 1);
 
-% training error for decision tree
-plot(TREE_MAX_DEPTHS', decisionTreeTrainErrors);
-xlabel('Max Depth');
-ylabel('Training Error');
-title('Decision Tree: Training Error vs. Ensemble Size');
-pause;
+    cnt = 1;
+    for size = ENSEMBLE_SIZES
+        fprintf('AdaBoost ensemble size: %d\n', size);
 
-% test error for decision tree
-plot(TREE_MAX_DEPTHS', decisionTreeTestErrors);
-xlabel('Max Depth');
-ylabel('Test Error');
-title('Decision Tree: Test Error vs. Ensemble Size');
-pause;
+        h = learnAdaBoost(XTrain, yTrain, size);
+        yTrainPred = inferAdaBoost(XTrain, h);
+        yTestPred = inferAdaBoost(XTest, h);
 
-%% (cleanup)
-rng('default') % reset seed
-clearvars -except *Results
+        eTrain = sum(yTrain ~= yTrainPred)/numel(yTrain);
+        aTrain = 1-eTrain;
+        fprintf('\tAdaBoost training error: %f\n', eTrain);
+
+        eTest = sum(yTest ~= yTestPred)/numel(yTest);
+        aTest = 1-eTest;
+        fprintf('\tAdaBoost test error: %f\n', eTest);
+
+        adaBoostResults{cnt}.ensembleSize = size;
+        adaBoostResults{cnt}.h = h;
+        adaBoostResults{cnt}.trainAccuracy = aTrain;
+        adaBoostResults{cnt}.trainRawConfusionMatrix = confusionmatrix(yTrain, yTrainPred, 1);
+        adaBoostResults{cnt}.trainConfusionMatrix = confusionmatrix(yTrain, yTrainPred);
+        adaBoostResults{cnt}.testAccuracy = aTest;
+        adaBoostResults{cnt}.testRawConfusionMatrix = confusionmatrix(yTest, yTestPred, 1);
+        adaBoostResults{cnt}.testConfusionMatrix = confusionmatrix(yTest, yTestPred);
+
+        adaBoostTrainErrors(cnt) = eTrain;
+        adaBoostTestErrors(cnt) = eTest;
+
+        cnt = cnt+1;
+    end
+
+    %% evaluate using decision tree
+    decisionTreeResults = cell(length(TREE_MAX_DEPTHS), 1);
+    decisionTreeTrainErrors = zeros(length(TREE_MAX_DEPTHS), 1);
+    decisionTreeTestErrors = zeros(length(TREE_MAX_DEPTHS), 1);
+
+    cnt = 1;
+    for depth = TREE_MAX_DEPTHS
+        fprintf('Decision tree max depth: %d\n', depth);
+
+        dist = 1/nTraining*ones(nTraining, 1);
+        h = learnDecisionTree(XTrain, yTrain, dist, depth);
+        yTrainPred = inferDecisionTree(XTrain, h);
+        yTestPred = inferDecisionTree(XTest, h);
+
+        eTrain = sum(yTrain ~= yTrainPred)/numel(yTrain);
+        aTrain = 1-eTrain;
+        fprintf('\tDecision tree training error: %f\n', eTrain);
+
+        eTest = sum(yTest ~= yTestPred)/numel(yTest);
+        aTest = 1-eTest;
+        fprintf('\tDecision tree test error: %f\n', eTest);
+
+        decisionTreeResults{cnt}.maxDepth = depth;
+        decisionTreeResults{cnt}.h = h;
+        decisionTreeResults{cnt}.trainAccuracy = aTrain;
+        decisionTreeResults{cnt}.trainRawConfusionMatrix = confusionmatrix(yTrain, yTrainPred, 1);
+        decisionTreeResults{cnt}.trainConfusionMatrix = confusionmatrix(yTrain, yTrainPred);
+        decisionTreeResults{cnt}.testAccuracy = aTest;
+        decisionTreeResults{cnt}.testRawConfusionMatrix = confusionmatrix(yTest, yTestPred, 1);
+        decisionTreeResults{cnt}.testConfusionMatrix = confusionmatrix(yTest, yTestPred);
+
+        decisionTreeTrainErrors(cnt) = eTrain;
+        decisionTreeTestErrors(cnt) = eTest;
+
+        cnt = cnt+1;
+    end
+
+    %% plot evaluations
+    % training error for adaboost
+    plot(ENSEMBLE_SIZES', adaBoostTrainErrors, 's--');
+    xlabel('Ensemble Size');
+    ylabel('Training Error');
+    title('AdaBoost: Training Error vs. Ensemble Size');
+    print('-djpeg', strcat(SAVE_FILENAME, '_boost_train.jpg'));
+    if PAUSE_PLOT
+        pause;
+    end
+
+    % test error for adaboost
+    plot(ENSEMBLE_SIZES', adaBoostTestErrors, 's--');
+    xlabel('Ensemble Size');
+    ylabel('Test Error');
+    title('AdaBoost: Test Error vs. Ensemble Size');
+    print('-djpeg', strcat(SAVE_FILENAME, '_boost_test.jpg'));
+    if PAUSE_PLOT
+        pause;
+    end
+
+    % training error for decision tree
+    plot(TREE_MAX_DEPTHS', decisionTreeTrainErrors, 's--');
+    xlabel('Max Depth');
+    ylabel('Training Error');
+    title('Decision Tree: Training Error vs. Ensemble Size');
+    print('-djpeg', strcat(SAVE_FILENAME, '_tree_train.jpg'));
+    if PAUSE_PLOT
+        pause;
+    end
+
+    % test error for decision tree
+    plot(TREE_MAX_DEPTHS', decisionTreeTestErrors, 's--');
+    xlabel('Max Depth');
+    ylabel('Test Error');
+    title('Decision Tree: Test Error vs. Ensemble Size');
+    print('-djpeg', strcat(SAVE_FILENAME, '_tree_test.jpg'));
+    if PAUSE_PLOT
+        pause;
+    end
+    
+    close all;
+    pause(1);
+
+    % save results
+    save(strcat(SAVE_FILENAME, '_results.mat'), 'adaBoostResults', 'decisionTreeResults');
+
+    %% (cleanup)
+    rng('default') % reset seed
+    clearvars -except iter
+
+%end
+
+clear;
